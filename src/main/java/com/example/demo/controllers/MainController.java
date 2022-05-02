203e6b612dbc7cafaef82a65d26bd6580a1745b2
Italo Costa <costa.italo.ti@gmail.com>
2022-05-02 11:58:02 -0300

    Adicionando post CSV no mainController

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.utilitarios.S3Util;
  
 @Controller
 public class MainController {
@@ -10,5 +16,25 @@ public class MainController {
     public String viewHomePage() {
         return "upload";
     }
-     
+    @PostMapping("/upload")
+    public String handleUploadForm(Model model, String description,
+            @RequestParam("file") MultipartFile multipart) {
+        String fileName = multipart.getOriginalFilename();
+         
+        System.out.println("Description: " + description);
+        System.out.println("filename: " + fileName);
+         
+        String message = "";
+         
+        try {
+            S3Util.uploadFile(fileName, multipart.getInputStream());
+            message = "Your file has been uploaded successfully!";
+        } catch (Exception ex) {
+            message = "Error uploading file: " + ex.getMessage();
+        }
+         
+        model.addAttribute("message", message);
+         
+        return "message";              
+    }
 }
