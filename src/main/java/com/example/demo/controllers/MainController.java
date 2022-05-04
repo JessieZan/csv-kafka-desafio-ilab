d0b3082c17ad471f55d27dabf3a313c7f27f3be5
Jessica Vargas Zanchetta <jessica.zanchetta@ifood.com.br>
2022-05-04 11:51:54 -0300

    funcionooooouuuuuuuuu

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,33 +1,48 @@
 package com.example.demo.controllers;
 
 
-import com.example.demo.model.Producer;
+import com.example.demo.model.KafkaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.utilitarios.S3Util;
- 
+
+import java.util.concurrent.ExecutionException;
+
 @Controller
 public class MainController {
- 
+
     @GetMapping("")
     public String viewHomePage() {
         return "upload";
     }
+
+
     @PostMapping("/upload")
     public String handleUploadForm(Model model, String description,
-            @RequestParam("file") MultipartFile multipart) {
+            @RequestParam("file") MultipartFile multipart) throws ExecutionException, InterruptedException {
+
+
         String fileName = multipart.getOriginalFilename();
          
         System.out.println("Description: " + description);
         System.out.println("filename: " + fileName);
          
         String message = "";
-         
+        try {
+            message = "Your file has been uploaded successfully!";
+            KafkaService.sendMessage("mensagem",message);
+
+        } catch (Exception ex) {
+            message = "Error uploading file: " + ex.getMessage();
+            System.out.println(message);
+        }
+
         try {
             S3Util.uploadFile(fileName, multipart.getInputStream());
 
@@ -37,36 +52,11 @@ public class MainController {
         }
          
         model.addAttribute("message", message);
-         
+
+        //KafkaService.sendMessage("mensagem",message);
+
         return "message";              
     }
 
-    @RequiredArgsConstructor
-    @RestController
-    @RequestMapping(value = "/kafka")
-    public class KafkaController {
-        private final Producer topicProducer;
-        @GetMapping (value = "/send")
-        public void send(@RequestParam("message") String message){
-            topicProducer.send(message);
-        }
-    }
-
-    /**@Controller
-    @RequestMapping(value = "/kafka")
-    public class KafkaController {
-
-        private final Producer producer;
-
-        @Autowired
-        KafkaController(Producer producer) {
-            this.producer = producer;
-        }
-
-        @PostMapping(value = "/publish")
-        public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
-            this.producer.sendMessage(message);
-        }
-    }**/
 
 }
