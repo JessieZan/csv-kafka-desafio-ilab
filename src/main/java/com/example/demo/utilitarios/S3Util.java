1a0953359b4010e82fe254c2650d48bc526c9916
Italo Costa <costa.italo.ti@gmail.com>
2022-05-10 13:51:47 -0300

    Atualizando upload

--- src/main/java/com/example/demo/utilitarios/S3Util.java
+++ src/main/java/com/example/demo/utilitarios/S3Util.java
@@ -3,27 +3,48 @@ package com.example.demo.utilitarios;
 import java.io.IOException;
 import java.io.InputStream;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.waiters.S3Waiter;
  
 public class S3Util {
-    private static final String BUCKET = "your-bucket-name";//colocar nome do nosso bucket
+    private static final String BUCKET = "group-3-bucket";
+   
      
     public static void uploadFile(String fileName, InputStream inputStream)
             throws S3Exception, AwsServiceException, SdkClientException, IOException {
-        S3Client client = S3Client.builder().build();
+        AwsCredentialsProvider credentialsProvider = new AwsCredentialsProvider() {
+      	  @Override
+      	  public AwsCredentials resolveCredentials() {
+      	      return new AwsCredentials() {
+      	          @Override
+      	          public String accessKeyId() {
+      	              return System.getenv("AWS_ACCESS_KEY");
+      	          }
+
+      	          @Override
+      	          public String secretAccessKey() {
+      	              return System.getenv("AWS_SECRET_KEY");
+      	          }
+      	      };
+      	  }
+      	};
+      	
+        S3Client client = S3Client.builder()                            
+                .region(Region.US_EAST_1)
+                .credentialsProvider(credentialsProvider)
+                .build();
+
          
         PutObjectRequest request = PutObjectRequest.builder()
                             .bucket(BUCKET)
-                            .key(fileName)// colocar nome do nosso arquivo
+                            .key(fileName)
                             .acl("public-read")
                             .build();
          
